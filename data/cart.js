8f5ea31f4d16c56f4220569db55af9deeca31783
Yukky <egorka0303@inbox.ru>
2025-11-06 00:36:40 +0300

    Create automated tests for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,16 +1,23 @@
-export let cart = JSON.parse(localStorage.getItem('cart')); 
+export let cart;
 
-if (!cart) {
-	cart =	[{
-		productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-		quantity: 2,
-        deliveryOptionId: "1"
-	}, {
-		productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-		quantity: 1,
-        deliveryOptionId: "2"
-	}];
+loadFromStorage();
+
+export function loadFromStorage() {
+    cart = JSON.parse(localStorage.getItem('cart')); 
+
+    if (!cart) {
+        cart =	[{
+            productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+            quantity: 2,
+            deliveryOptionId: "1"
+        }, {
+            productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+            quantity: 1,
+            deliveryOptionId: "2"
+        }];
+    }
 }
+
 
 function saveToStorage() {
 	localStorage.setItem('cart', JSON.stringify(cart));
@@ -18,7 +25,7 @@ function saveToStorage() {
 
 let addedTimer;
 
-export function addToCart(productId) {
+export function addToCartLogic(productId, quantity) {
     let matchingItem;
 
     cart.forEach((cartItem) => {
@@ -27,6 +34,21 @@ export function addToCart(productId) {
         }
     });
 
+    if (matchingItem) {
+        matchingItem.quantity++;
+    } else {
+        cart.push({
+            productId,
+            quantity: Number(quantity),
+            deliveryOptionId: "1"
+        });
+    }
+
+	saveToStorage();
+    return matchingItem ? 'updated' : 'added'
+}
+
+export function addToCart(productId) {
     const addedToCart = document.querySelector(`.js-added-to-cart-${productId}`)
     const selectedQuantity = document.querySelector(`.js-quantity-selector-${productId}`)
 
@@ -38,17 +60,8 @@ export function addToCart(productId) {
         addedToCart.classList.remove('added-message');
     }, 2000);
 
-    if(matchingItem) {
-        matchingItem.quantity++;
-    } else {
-        cart.push({
-            productId,
-            quantity: Number(selectedQuantity.value),
-            deliveryOptions: "1"
-        });
-    }
-
-	saveToStorage();
+    const quantity = Number(selectedQuantity.value);
+    return addToCartLogic(productId, quantity)
 }
 
 export function removeFromCart(productId) {
