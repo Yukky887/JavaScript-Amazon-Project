0aa88bbd6bcefa37695ff365694e7b518432a72e
Yukky <egorka0303@inbox.ru>
2025-11-11 22:31:36 +0300

    17L.2 OOP Car

--- data/car.js
+++ data/car.js
@@ -2,38 +2,78 @@ class Car {
     brand;
     model;
     speed;
+    isTrunkOpen;
     constructor(brand, model) {
         this.brand = brand;
         this.model = model;
+        this.speed = 0;
+        this.isTrunkOpen = false;
     }  
-
-    displayInfo() {
-        console.log(`${this.brand} ${this.model}, Speed: ${this.speed} km/h`)
+    
+    openTrunk() {
+        if (this.isTrunkOpen || this.speed > 0) return
+       
+        this.isTrunkOpen = true;
+    }
+    
+    closeTrunk() {
+        if (!this.isTrunkOpen || this.speed > 0) return
+        
+        this.isTrunkOpen = false;
     }
 
     go() {
-        if (this.speed <= 0 || this.speed >= 200) return
-
+        if (this.speed < 0 || this.speed >= 200 || this.isTrunkOpen) return
+        
         this.speed += 5;
     }
-
+    
     break() {
         if (this.speed <= 0) return
         
         this.speed -= 5;
+    }
+    
+    displayInfo() {
+        console.log(`${this.brand} ${this.model}, Speed: ${this.speed} km/h, Trunk is ${this.isTrunkOpen ? 'open' : 'closed'}`);
+    }
+}
+
+class SportCar extends Car {
+
+    constructor(brand, model) {
+        super(brand, model);
+    }
+
+    go() {
+        if (this.speed < 0 || this.speed >= 300) return
+
+        this.speed += 20;
     }
 }
 
 
 
 const car = new Car("Bmw", 'e34')
-const car2 = new Car("Toyota", "Corolla")
-car.go();
-car.go();
-car.go();
-car.go();
-car.go();
-car.go();
-car.go();
-car.go();
+const sportCar = new SportCar("Porsche", "911-GT3")
+car.openTrunk();
 car.displayInfo();
+
+sportCar.go();
+sportCar.go();
+sportCar.go();
+sportCar.go();
+sportCar.go();
+sportCar.go();
+sportCar.go();
+sportCar.go();
+sportCar.go();
+sportCar.go();
+sportCar.go();
+sportCar.go();
+sportCar.go();
+sportCar.go();
+sportCar.go();
+sportCar.go();
+
+sportCar.displayInfo();
